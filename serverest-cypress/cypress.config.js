31118b6f75a85462cc381f9b69dcbf1eaa13801a
Yann Pereira <yann.srn@gmail.com>
2026-05-10 20:41:21 -0300

    Cenário Login via API

--- serverest-cypress/cypress.config.js
+++ serverest-cypress/cypress.config.js
@@ -3,12 +3,21 @@ const { defineConfig } = require('cypress')
 module.exports = defineConfig({
 
   e2e: {
-    frontBaseUrl: 'https://front.serverest.dev',
-    
+    baseUrl: 'https://front.serverest.dev',
+    viewportWidth: 1280,
+    viewportHeight: 720,
+    defaultCommandTimeout: 10000,
+    requestTimeout: 15000,
+    responseTimeout: 15000,
+    video: false,
+    screenshotOnRunFailure: true,
+    specPattern: 'cypress/e2e/**/*.cy.js',
+    supportFile: 'cypress/support/e2e.js',
     env: {
       apiBaseUrl: 'https://serverest.dev',
     },
     setupNodeEvents(on, config) {
+      // eventos Node podem ser registrados aqui
       return config
     },
   },
---
+++ serverest-cypress/cypress/e2e/api/login.cy.js
@@ -0,0 +1,47 @@
+describe('Cenário API: Autenticação', () => {
+  const API = Cypress.env('apiBaseUrl')
+
+  context('Credenciais válidas', () => {
+    it('deve retornar status 200 e um token Bearer ao autenticar com sucesso', () => {
+      cy.fixture('usuarios').then(({ usuarioValido }) => {
+        cy.request({
+          method: 'POST',
+          url: `${API}/login`,
+          body: {
+            email: usuarioValido.email,
+            password: usuarioValido.password,
+          },
+        }).then((response) => {
+          expect(response.status).to.eq(200)
+          expect(response.body).to.have.property('message', 'Login realizado com sucesso')
+          expect(response.body).to.have.property('authorization').and.to.be.a('string')
+
+        })
+      })
+    })
+  })
+context('Credenciais inválidas', () => {
+    it('deve retornar status 401 ao informar senha incorreta e não realizar login', () => {
+      cy.fixture('usuarios').then(({ usuarioInvalido }) => {
+        cy.request({
+          method: 'POST',
+          url: `${API}/login`,
+          body: {
+            email: usuarioInvalido.email,   // e-mail correto
+            password: usuarioInvalido.password, // senha errada
+          },
+          failOnStatusCode: false,
+        }).then((response) => {
+          expect(response.status).to.eq(401)
+          expect(response.body).to.have.property(
+            'message',
+            'Email e/ou senha inválidos'
+          )
+          expect(response.body).not.to.have.property('authorization')
+        })
+      })
+    })
+  })
+})
+
+
